053fddc4cb8d63417a73fe0746e7b55ff55e1ddc
HarrisonWismer <harrisonjwismer@gmail.com>
2025-03-06 15:12:31 -0800

    Data Path

--- quipi_humu/box_viol_expression_humu.py
+++ quipi_humu/box_viol_expression_humu.py
@@ -9,7 +9,7 @@ import shared as sh
 
 def box_humu(score1, score2, x_cat, x_cat_filts, group):
     cols = set([col for col in [score1, score2, x_cat, group] if col in sh.flow_cats + sh.flow_scores])
-    flow_table = pd.read_feather("/Users/hwismer/Documents/QuIPI/quipi_humu/quipi_humu_data/quipi_humu_flow_table.feather", columns=cols)
+    flow_table = pd.read_feather("./quipi_humu/quipi_humu_data/quipi_humu_flow_table.feather", columns=cols)
     flow_table = flow_table[flow_table[x_cat].isin(x_cat_filts)]
 
     if group in sh.flow_cats and group != x_cat:
